ebc8fceb13bc433144e5bcacdf34a45874604ce8
Joshua Mankelow <joshua@mankelow.org>
2022-06-16 21:59:39 +0100

    commit to test :)

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Docker & GitHub actions test</h1>"
+    return "<h1>Docker & GitHub actions test!</h1>"
 
 
 if __name__ == "__main__":
