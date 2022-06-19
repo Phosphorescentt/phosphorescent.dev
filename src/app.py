ea493bf35616c351592addf924eb868812a44ad2
Joshua Mankelow <joshua@mankelow.org>
2022-06-20 00:34:29 +0100

    major update so now there's actually styling or something idk

--- src/app.py
+++ src/app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route("/")
 def index():
-    return "<h1>Docker & GitHub actions test!</h1><br><h4>Hi Caity!</h4>"
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
